04d95f9c5d2d0ab538fd7513ff794c4c810a051f
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2021-11-28 17:11:02 +0100

    setup finished

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import VueChatScroll from "vue-chat-scroll";
+import firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.use(VueChatScroll);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB-QQtG8mzcybXY_3zhMLFI1n6IHKQMp_g",
+  authDomain: "rtc-26e71.firebaseapp.com",
+  projectId: "rtc-26e71",
+  storageBucket: "rtc-26e71.appspot.com",
+  messagingSenderId: "150939648310",
+  appId: "1:150939648310:web:93933a68d22f8d65187397"
+});
+
+export const db = firebase.firestore();
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloWorld msg="Das ist die About-Page" />
+    <Home />
   </div>
 </template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+import Home from "./Home.vue";
+
+export default {
+  components: { HelloWorld, Home },
+};
+</script>
