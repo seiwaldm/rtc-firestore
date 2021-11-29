bdaaa869407bec7452b6212e04beff6042d48f27
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2021-11-29 12:21:42 +0100

    cont

---
+++ src/components/Chat.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container chat">
+    <div class="center">
+      <router-link to="/" class="btn green">Back to Home</router-link>
+    </div>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages" v-chat-scroll>
+          <li
+            class="chat-message"
+            v-for="message in messages"
+            :key="message.id"
+          >
+            <div class="right-bubble" v-if="message.name === name"></div>
+            <div class="left-bubble right-align"></div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row">
+    <div class="col s8 offset-s2">
+      <div class="container">
+        <div class="card">
+          <div class="card-content center-align">
+            <form @submit.prevent="enterChat">
+              <h4>Enter Your Name:</h4>
+              <input type="text" v-model="name" />
+              <p v-if="feedback">{{ feedback }}</p>
+              <button class="btn green">Enter Chat</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: function () {
+    return {
+      name: null,
+      feedback: null,
+    };
+  },
+  methods: {
+    enterChat: function () {
+      if (this.name) {
+        this.$router.push({
+          name: "Chat",
+          params: {
+            name: this.name,
+          },
+        });
+      } else {
+        this.feedback = "Please enter a name!";
+      }
+    },
+  },
+};
+</script>
