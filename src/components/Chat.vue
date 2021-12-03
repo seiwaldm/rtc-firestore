f605c6b4fb04bd133b0e4042608e018cfd9c8426
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2021-12-03 10:21:05 +0100

    Update Chat.vue

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -11,11 +11,39 @@
             v-for="message in messages"
             :key="message.id"
           >
-            <div class="right-bubble" v-if="message.name === name"></div>
-            <div class="left-bubble right-align"></div>
+            <div class="right-bubble" v-if="message.name === name">
+              <span class="green-text"> {{ message.name }}&nbsp;</span>
+              <span class="grey-text text-darken-3">
+                {{ message.content }}</span
+              >
+              <span class="grey-text time">
+                {{ message.timestamp | moment }}</span
+              >
+            </div>
+            <div class="left-bubble right-align" v-if="message.name !== name">
+              <span class="green-text"> {{ message.name }}&nbsp;</span>
+              <span class="grey-text text-darken-3">
+                {{ message.content }}</span
+              >
+              <span class="grey-text time">
+                {{ message.timestamp | moment }}</span
+              >
+            </div>
           </li>
         </ul>
+      </div>
+      <div class="card-content">
+        <form @submit.prevent="addMessage">
+          <label for="chatMessage">New Message from {{ name }}:</label>
+          <input type="text" id="chatMessage" v-model="newMessage" />
+          <p v-if="feedback">{{ feedback }}</p>
+          <button class="btn green">Send Message!</button>
+        </form>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
