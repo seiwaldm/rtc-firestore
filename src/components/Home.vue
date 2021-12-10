34ec0bbfe5cf35b1a084c2cbf77dfbce5ce44844
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2021-12-10 20:38:29 +0100

    sign in with google

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col s8 offset-s2">
       <div class="container">
-        <div class="card">
+        <div class="card" v-if="loggedIn">
           <div class="card-content center-align">
             <form @submit.prevent="enterChat">
               <h4>Enter Your Name:</h4>
@@ -12,20 +12,31 @@
             </form>
           </div>
         </div>
+        <div class="card" v-if="!loggedIn">
+          <div class="card-content center-align">
+            <button class="btn green" @click="googleSignIn">
+              Sign In with Google
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+
 export default {
   name: "Home",
   data: function () {
     return {
       name: null,
       feedback: null,
+      loggedIn: false,
     };
   },
+
   methods: {
     enterChat: function () {
       if (this.name) {
@@ -37,6 +48,16 @@ export default {
         });
       } else {
         this.feedback = "Please enter a name!";
+      }
+    },
+    async googleSignIn() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      try {
+        const response = await firebase.auth().signInWithPopup(provider);
+        console.log(response);
+        this.loggedIn = true;
+      } catch (error) {
+        alert(error);
       }
     },
   },
